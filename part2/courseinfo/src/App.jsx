a01b240c43366aebd8cf69b5bc7f8d7897448685
zakerous <m.z.humayoon@gmail.com>
2025-07-17 10:19:01 +0300

    Submitting 2.1: Course information step 6

---
+++ part2/courseinfo/src/App.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+    </div>
+  );
+};
+
+const Header = (props) => <h1>{props.course}</h1>;
+
+const Content = (props) => (
+  <div>
+    {props.parts.map((x) => (
+      <Part part={x} />
+    ))}
+  </div>
+);
+
+const Part = (props) => (
+  <p>
+    {props.part.name} {props.part.exercises}
+  </p>
+);
+
+const Total = (props) => (
+  <p>
+    <b>Number of exercises: {props.total}</b>
+  </p>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+export default App;

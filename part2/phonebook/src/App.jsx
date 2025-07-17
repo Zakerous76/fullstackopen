314ea6771b10b79f1fe712b31a1035fbbcd1bcdc
zakerous <m.z.humayoon@gmail.com>
2025-07-17 13:13:44 +0300

    2.9*: The Phonebook Step 4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -16,10 +16,17 @@ const Numbers = ({ persons }) => {
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas", number: "552 555 55 55" },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
+
+  const [visiblePersons, setVisiblePersons] = useState(persons);
+
   const [newName, setNewName] = useState(""); // for controlling <input/>
   const [newNumber, setNewNumber] = useState("");
+  const [searchTerm, setSearchTerm] = useState("");
 
   const addNewName = (event) => {
     event.preventDefault();
@@ -46,9 +53,32 @@ const App = () => {
     setNewNumber(event.target.value);
   };
 
+  const handleSearch = (event) => {
+    const newSearchTerm = event.target.value;
+    setSearchTerm(newSearchTerm);
+
+    const newVisiblePersons = persons.filter((person) => {
+      return person.name.toLowerCase().includes(newSearchTerm.toLowerCase());
+    });
+
+    console.log(newVisiblePersons);
+    setVisiblePersons(newVisiblePersons);
+  };
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div id="searchSection">
+        <label htmlFor="searchBox">Filter shown with: </label>
+        <input
+          type="text"
+          id="searchBox"
+          placeholder="Enter name..."
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </div>
+      <h2>Add a new entry</h2>
       <form onSubmit={addNewName}>
         <div>
           <label htmlFor="name">name: </label>
@@ -64,9 +94,9 @@ const App = () => {
           <input
             type="tel"
             id="phoneNo"
-            placeholder="552 555 55 55"
-            maxLength={10}
-            pattern="[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}"
+            placeholder="12-12-1234567"
+            // maxLength={10}
+            // pattern="[0-9]{3} [0-9]{3} [0-9]{2} [0-9]{2}"
             value={newNumber}
             onChange={handleNewNumber}
           />
@@ -76,7 +106,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <Numbers persons={persons} />
+      <Numbers persons={visiblePersons} />
     </div>
   );
 };

e6538bd5dbfc7aaee814c0546e0627ba94be7dc0
zakerous <m.z.humayoon@gmail.com>
2025-07-21 08:36:34 +0300

    3.3: Phonebook backend step 3

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -58,6 +58,17 @@ app.get("/info", (req, res) => {
   res.status(200).send(responseString);
 });
 
+// GET a person
+app.get("/api/persons/:id", (req, res) => {
+  const personId = req.params.id;
+  const person = phonebook.find((p) => p.id === personId);
+  if (person) {
+    res.status(200).json(person);
+    return;
+  }
+  res.status(404).json({ error: `Person with ID:${personId} not found` });
+});
+
 // Start server
 app.listen(PORT, () => {
   console.log(`Server Started\nListening to Port ${PORT}.`);

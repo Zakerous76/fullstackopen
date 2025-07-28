2ec568b9501b6c6493ac98426e5fdd2f243c20f7
zakerous <m.z.humayoon@gmail.com>
2025-07-28 15:40:04 +0300

    idk what i am doing

--- part4 and part5/bloglist-frontend/src/tests/Blog.test.js
+++
@@ -1,7 +0,0 @@
-// check wether the blog's title and author, but does not render its URL or number of likes by default.
-import render
-
-test("renders the blog's title and author but not its URL and the number of likes", () => {
-  const container = render
-
-});

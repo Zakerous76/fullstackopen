5a9f5efb4a8a6927a93ed8b0cf5cce2e9f1de6c4
zakerous <m.z.humayoon@gmail.com>
2025-08-09 08:33:40 +0300

    8.5: Books by genre

--- part8/library-backend/index.js
+++ part8/library-backend/index.js
@@ -117,10 +117,11 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
-    allBooks(author: String): [Book!]!
+    allBooks(author: String genre: String): [Book!]!
     allAuthors: [Author]
-
   }
+
+
 `
 
 const resolvers = {
@@ -128,10 +129,18 @@ const resolvers = {
     bookCount: () => books.length,
     authorCount: () => authors.length,
     allBooks: (root, args) => {
-      if (!args.author) {
+      if (!args.author && !args.genre) {
         return books
       }
-      return books.filter((b) => b.author === args.author)
+      if (args.author && !args.genre) {
+        return books.filter((b) => b.author === args.author)
+      }
+      if (!args.author && args.genre) {
+        return books.filter((b) => b.genres.includes(args.genre))
+      }
+      return books.filter(
+        (b) => b.author === args.author && b.genres.includes(args.genre)
+      )
     },
     allAuthors: () => {
       return authors.map((author) => {

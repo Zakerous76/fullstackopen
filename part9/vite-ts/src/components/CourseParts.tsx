4d0066b6a46bb762a08412d5afa8e00e4c928ea5
zakerous <m.z.humayoon@gmail.com>
2025-08-28 12:34:15 +0300

    Exercise 9.15

---
+++ part9/vite-ts/src/components/CourseParts.tsx
@@ -0,0 +1,24 @@
+interface Content {
+  courseParts: Array<{
+    name: string
+    exerciseCount: number
+  }>
+}
+
+const Content = (props: Content) => {
+  return (
+    <div>
+      <p>
+        {props.courseParts[0].name} {props.courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[1].name} {props.courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {props.courseParts[2].name} {props.courseParts[2].exerciseCount}
+      </p>
+    </div>
+  )
+}
+
+export default Content
---
+++ part9/vite-ts/src/components/Total.tsx
@@ -0,0 +1,8 @@
+interface TotalProps {
+  totalExercises: number
+}
+const Total = (props: TotalProps) => {
+  return <p>Number of exercises {props.totalExercises}</p>
+}
+
+export default Total

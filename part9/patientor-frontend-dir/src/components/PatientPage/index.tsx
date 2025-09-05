d8bbe45d1034e9f22820b26d7b22e95e81fe4590
zakerous <m.z.humayoon@gmail.com>
2025-09-05 12:00:24 +0300

    9.26: Patientor, step 6

--- part9/patientor-frontend-dir/src/components/PatientPage/index.tsx
+++ part9/patientor-frontend-dir/src/components/PatientPage/index.tsx
@@ -1,60 +1,153 @@
-import { Box, Typography } from "@mui/material"
+import { Box, Paper, Typography } from "@mui/material"
 import { Entry, Gender, Patient } from "../../types"
-import { Female, Lightbulb, Male } from "@mui/icons-material"
+import {
+  Favorite,
+  Female,
+  Lightbulb,
+  LocalHospital,
+  Male,
+  MedicalServices,
+} from "@mui/icons-material"
 import diagnosisService from "../../services/diagnosis"
+import WorkIcon from "@mui/icons-material/Work"
 
 interface PatientProps {
   patient: Patient | undefined
 }
 
-const PatientPage = (props: PatientProps) => {
+// utility to ensure exhaustive type checking
+const assertNever = (value: never): never => {
+  throw new Error(`Unhandled entry type: ${JSON.stringify(value)}`)
+}
+
+// component to render entries by type
+const EntryDetails = ({ entry }: { entry: Entry }) => {
+  switch (entry.type) {
+    case "HealthCheck":
+      return (
+        <Paper elevation={4} key={entry.id} sx={{ p: "10px", my: "4px" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography component="span">{entry.date} </Typography>
+            <MedicalServices />
+          </Box>
+          <Typography fontStyle="italic" variant="subtitle1" my={1}>
+            {entry.description}
+          </Typography>
+          <Box>
+            {entry.healthCheckRating === 0 ? (
+              <Favorite sx={{ color: "green" }} />
+            ) : entry.healthCheckRating === 1 ? (
+              <Favorite htmlColor={"yellow"} />
+            ) : entry.healthCheckRating === 2 ? (
+              <Favorite htmlColor={"orange"} />
+            ) : (
+              <Favorite htmlColor={"red"} />
+            )}
+          </Box>
+          {entry.diagnosisCodes?.map((code) => (
+            <ul key={`${entry.id + code}`}>
+              <li>
+                {diagnosisService.getDiagnosisFromCode(code).code}{" "}
+                {diagnosisService.getDiagnosisFromCode(code).name}
+              </li>
+            </ul>
+          ))}
+          <Typography>diagnose by {entry.specialist}</Typography>
+        </Paper>
+      )
+
+    case "OccupationalHealthcare":
+      return (
+        <Paper elevation={4} key={entry.id} sx={{ p: "10px", my: "4px" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography component="span">{entry.date} </Typography>
+            <Typography component="span" fontStyle="italic">
+              <WorkIcon /> {entry.employerName}
+            </Typography>
+          </Box>
+          <Typography fontStyle="italic" variant="subtitle1" my={1}>
+            {entry.description}
+          </Typography>
+          {entry.diagnosisCodes?.map((code) => (
+            <ul key={`${entry.id + code}`}>
+              <li>
+                {diagnosisService.getDiagnosisFromCode(code).code}{" "}
+                {diagnosisService.getDiagnosisFromCode(code).name}
+              </li>
+            </ul>
+          ))}
+          <Typography>diagnose by {entry.specialist}</Typography>
+        </Paper>
+      )
+
+    case "Hospital":
+      return (
+        <Paper elevation={4} key={entry.id} sx={{ p: "10px", my: "4px" }}>
+          <Box sx={{ display: "flex", alignItems: "center" }}>
+            <Typography component="span">{entry.date} </Typography>
+            <LocalHospital />
+          </Box>
+          <Typography fontStyle="italic" variant="subtitle1" my={1}>
+            {entry.description}
+          </Typography>
+          {entry.discharge && (
+            <Typography variant="body2">
+              Discharged on {entry.discharge.date}: {entry.discharge.criteria}
+            </Typography>
+          )}
+          {entry.diagnosisCodes?.map((code) => (
+            <ul key={`${entry.id + code}`}>
+              <li>
+                {diagnosisService.getDiagnosisFromCode(code).code}{" "}
+                {diagnosisService.getDiagnosisFromCode(code).name}
+              </li>
+            </ul>
+          ))}
+          <Typography>diagnose by {entry.specialist}</Typography>
+        </Paper>
+      )
+
+    default:
+      return assertNever(entry)
+  }
+}
+
+const PatientPage = ({ patient }: PatientProps) => {
+  if (!patient) return null
+
   return (
     <Box>
       <Box>
         <Typography component="h2" variant="h4" fontWeight={600} py={2}>
-          {props.patient?.name}{" "}
-          {props.patient?.gender === Gender.Male ? (
+          {patient.name}{" "}
+          {patient.gender === Gender.Male ? (
             <Male sx={{ fontSize: "30px" }} />
-          ) : props.patient?.gender === Gender.Female ? (
+          ) : patient.gender === Gender.Female ? (
             <Female sx={{ fontSize: "30px" }} />
           ) : (
             <Lightbulb sx={{ fontSize: "30px" }} />
           )}
         </Typography>
-        <Typography variant="body1">ssh: {props.patient?.ssn}</Typography>
+        <Typography variant="body1">ssh: {patient.ssn}</Typography>
         <Typography variant="body1">
-          occupation: {props.patient?.occupation}
+          occupation: {patient.occupation}
         </Typography>
       </Box>
+
       <Box>
-        {props.patient?.entries ? (
-          props.patient.entries.length > 0 ? (
-            <Typography
-              component="h3"
-              variant="h5"
-              fontWeight={600}
-              pt={5}
-              pb={1}
-            >
-              {" "}
-              Entries
-            </Typography>
-          ) : null
-        ) : null}
-        {props.patient?.entries.map((entry: Entry) => (
-          <Box key={entry.id}>
-            <Typography variant="body1" my={0}>
-              {entry.date} {entry.description}
-            </Typography>
-            <ul>
-              {entry.diagnosisCodes?.map((code) => (
-                <li key={`${entry.id + code}`}>
-                  {diagnosisService.getDiagnosisFromCode(code).code}{" "}
-                  {diagnosisService.getDiagnosisFromCode(code).name}
-                </li>
-              ))}
-            </ul>
-          </Box>
+        {patient.entries.length > 0 && (
+          <Typography
+            component="h3"
+            variant="h5"
+            fontWeight={600}
+            pt={5}
+            pb={1}
+          >
+            Entries
+          </Typography>
+        )}
+        {patient.entries.map((entry) => (
+          <EntryDetails key={entry.id} entry={entry} />
         ))}
       </Box>
     </Box>
